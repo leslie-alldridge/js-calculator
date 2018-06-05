b582fc6b7cfae6412df506df7e7c30fceca39547
Leslie Alldridge <Leslie@Leslie-Alldridges-MacBook-Air.local>
2018-06-05 19:01:23 +1200

    working on passing symbols through

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-
+//set global variables 
     var current,
         totalview,
         output,
@@ -7,15 +7,15 @@ window.onload = function() {
         zero,
         period,
         operator;
-        
+//grab the total display and set to variable
         totalview = document.getElementById("totaldisplay");
-    
-            var allBtn = document.querySelectorAll(".calcbutton");
+
+        var allBtn = document.querySelectorAll(".calcbutton");
         
           var len = allBtn.length;
         
           for(var i = 0; i < len; i++ ) {
-            
+            // for every button add a click listener
             allBtn[i].addEventListener("click",function() {
                       
                 num = this.value;
@@ -29,8 +29,26 @@ window.onload = function() {
              alert("You've outgrown this calculator");
              totalview.innerHTML = '';       
            }
+            
+          
            
          },false);
             
-        } 
+        } // end of for loop looping thru all buttons
+        var operator = document.querySelectorAll(".calcbuttonsymbol");
+        //symbols
+        for(var n = 0; n < operator.length; n++ ) {
+            // for every button add a click listener
+            operator[n].addEventListener("click",function() {
+                      
+                symbol = this.value;
+                         
+                alert(symbol);     // this works but doesn't show symbol, should be ok 
+            
+            
+        
+           
+         },false);
+            
+        }
     }
